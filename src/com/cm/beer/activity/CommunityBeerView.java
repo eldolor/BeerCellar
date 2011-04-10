f14b0267284fffb5babb511f1f2020120bc8e893
Anshu <anshu.gaind@gmail.com>
2011-04-09 23:49:51 -0500

    .

--- src/com/cm/beer/activity/CommunityBeerView.java
+++ src/com/cm/beer/activity/CommunityBeerView.java
@@ -1037,37 +1037,37 @@ public class CommunityBeerView extends Activity {
 
 	}
 
-	private void setupReviewHelpfulCount() {
-		mReviewHelpfulCountHandler = new Handler() {
-			@Override
-			public void handleMessage(Message message) {
-				String jsonStr = (String) message.obj;
-				Log.i(TAG, jsonStr);
-				if ((jsonStr != null) && (jsonStr.startsWith("{"))) {
-					JSONObject json;
-					try {
-						json = new JSONObject(jsonStr);
-						int yes = json.getInt("yes");
-						int no = json.getInt("no");
-						String _reviewHelpfulMessage = yes + " out of "
-								+ (yes + no) + " found this review helpful:";
-						Log.d(TAG, _reviewHelpfulMessage);
-						// mReviewHelpfulMessage.setText(_reviewHelpfulMessage);
-					} catch (JSONException e) {
-						Log.e(TAG, "error: "
-								+ ((e.getMessage() != null) ? e.getMessage()
-										.replace(" ", "_") : ""), e);
-					}
-				} else {
-					// mReviewHelpfulMessage.setVisibility(View.GONE);
-				}
-			}
-		};
-		mContentManager.fetchContentOnThread(Util
-				.getReviewHelpfulCountUrl(mCommunityBeer.beerId),
-				mReviewHelpfulCountHandler);
-
-	}
+//	private void setupReviewHelpfulCount() {
+//		mReviewHelpfulCountHandler = new Handler() {
+//			@Override
+//			public void handleMessage(Message message) {
+//				String jsonStr = (String) message.obj;
+//				Log.i(TAG, jsonStr);
+//				if ((jsonStr != null) && (jsonStr.startsWith("{"))) {
+//					JSONObject json;
+//					try {
+//						json = new JSONObject(jsonStr);
+//						int yes = json.getInt("yes");
+//						int no = json.getInt("no");
+//						String _reviewHelpfulMessage = yes + " out of "
+//								+ (yes + no) + " found this review helpful:";
+//						Log.d(TAG, _reviewHelpfulMessage);
+//						// mReviewHelpfulMessage.setText(_reviewHelpfulMessage);
+//					} catch (JSONException e) {
+//						Log.e(TAG, "error: "
+//								+ ((e.getMessage() != null) ? e.getMessage()
+//										.replace(" ", "_") : ""), e);
+//					}
+//				} else {
+//					// mReviewHelpfulMessage.setVisibility(View.GONE);
+//				}
+//			}
+//		};
+//		mContentManager.fetchContentOnThread(Util
+//				.getReviewHelpfulCountUrl(mCommunityBeer.beerId),
+//				mReviewHelpfulCountHandler);
+//
+//	}
 
 	public void handleUserNotLoggedInFacebook() {
 		Log.i(TAG, "handleUserNotLoggedInFacebook");
